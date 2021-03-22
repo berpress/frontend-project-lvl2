fb368253fd9e04cc8c3e3419965a1b579407f069
Alexander <berpress@gmail.com>
2021-03-22 13:39:34 +0300

    Fix after review

--- src/ast.js
+++ src/ast.js
@@ -1,7 +1,6 @@
 import _ from 'lodash';
 import path from 'path';
-// eslint-disable-next-line import/extensions
-import { jsonParser, yamlParser } from './parsers.js';
+import { jsonParser, yamlParser } from './parsers';
 
 const getChangedObject = (keys, obj, type) => keys.map((key) => ({
   children: obj[key], status: type, name: key,
